abb4f4619fc39b83100d3f1452e99add13cd0e11
Rajvi Detroja <130290425+kevit-rajvee-detroja@users.noreply.github.com>
2023-09-04 15:22:41 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location , {mode:'cors'}).then((response) => {
+    fetch('https://weather-app-zjqo.onrender.com/weather?address=' + location , {mode:'cors'}).then((response) => {
         response.json().then((data) => {
             if(data.error) {
                 messageOne.textContent = data.error
